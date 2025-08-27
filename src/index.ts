8ecefcfc31318ae29938a10bb0517f6122b767d6
aricionur <aricionur@gmail.com>
2025-08-27 16:05:04 +0100

    add dotenv to load env file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import express, { Request, Response } from 'express';
 import taskRouter from './routes/taskRoutes';
+import dotenv from 'dotenv';
 
-console.log("** ",process.env.PORT)
+// Load env file
+dotenv.config();
 
-const app = express();
 const port = process.env.PORT || 3000;
 const apiVersion = "v1";
 
-// Middleware to parse JSON
-app.use(express.json());
+const app = express();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from the backend!');
 });
 
-// Register task routes
+// Middleware to parse JSON
+app.use(express.json());
+
+// Middleware for task routes
 app.use(`/${apiVersion}`, taskRouter);
 
 app.listen(port, () => {
