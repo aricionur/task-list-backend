b291af883ee3fe86d7d33d5c46ba770e5a363b78
aricionur <aricionur@gmail.com>
2025-09-01 21:10:56 +0100

    add swagger API documentation

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express, { NextFunction, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import fs from "fs";
+import yaml from "js-yaml";
+
 import { PORT, API_VERSION } from "./constants";
 import { initializeDatabase } from "./db/createTables";
 import { logError } from "./logging/logger";
 import { checkDatabaseConnection } from "./db/dbConnection";
-
 import { TaskService } from "./services/TaskService";
 import { createTaskRouter } from "./routes/taskRoutes";
 
@@ -14,6 +18,12 @@ checkDatabaseConnection();
 initializeDatabase();
 
 const app = express();
+
+// Load OpenAPI specification from YAML file using an absolute path
+const openApiDocument = yaml.load(fs.readFileSync(path.join(__dirname, "../openapi.yaml"), "utf8")) as object;
+
+// Serve Swagger documentation
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openApiDocument));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("API Version: " + API_VERSION);
@@ -23,13 +33,12 @@ app.get("/", (req: Request, res: Response) => {
 app.use(express.json());
 
 // Register routes
-// app.use(`/${API_VERSION}`, taskRouter);
 const taskService = new TaskService();
 app.use(`/${API_VERSION}`, createTaskRouter(taskService));
 
 // Register a generic error handler
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  logError("Unhandled server error", err); // Send the error to the cloud service
+  logError("Unhandled server error", err); // Send the error to a cloud service for monitoring
   res.status(500).send("Oops! Something went wrong!");
 });
 
