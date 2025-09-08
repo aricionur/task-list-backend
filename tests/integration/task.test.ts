db09ea33ba718a38df2c80948dbc5961bacc9416
aricionur <aricionur@gmail.com>
2025-09-08 16:36:22 +0100

    Add Joi validation tests

--- tests/integration/task.test.ts
+++ tests/integration/task.test.ts
@@ -34,6 +34,9 @@ describe("Task API Integration Tests (In-Memory DB)", () => {
     await testDataSource.destroy();
   });
 
+  // ----------------------------
+  //       CRUD tests
+  // ----------------------------
   it(`POST /${API_VERSION}/task should create a task`, async () => {
     // ARRANGE
     const payload = { title: "In-Memory Test Task", status: "Todo" };
@@ -92,5 +95,57 @@ describe("Task API Integration Tests (In-Memory DB)", () => {
 
     // ASSERT
     expect(deleted).toBeNull();
+  });
+
+  // ----------------------------
+  //    Joi Validation Tests
+  // ----------------------------
+  describe("Joi Validation Tests", () => {
+    it(`POST /${API_VERSION}/task should fail without required fields`, async () => {
+      const invalidPayload = { description: "No title or status" };
+
+      const response = await request(app).post(`/${API_VERSION}/task`).send(invalidPayload);
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors).toContain('"title" is required');
+      expect(response.body.errors).toContain('"status" is required');
+    });
+
+    it(`POST /${API_VERSION}/task should fail with invalid status`, async () => {
+      const invalidPayload = { title: "Task", status: "InvalidStatus" };
+
+      const response = await request(app).post(`/${API_VERSION}/task`).send(invalidPayload);
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors[0]).toContain('"status" must be one of');
+    });
+
+    it(`PUT /${API_VERSION}/task/:id should fail with invalid ID`, async () => {
+      const id = "abc";
+      const task = { title: "New Title" };
+
+      const response = await request(app).put(`/${API_VERSION}/task/${id}`).send(task);
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors[0]).toContain('"id" must be a number');
+    });
+
+    it(`PUT /${API_VERSION}/task/:id should fail with invalid status`, async () => {
+      const task = await testDataSource.getRepository(Task).save({ title: "Task", status: "Todo" });
+
+      const response = await request(app).put(`/${API_VERSION}/task/${task.id}`).send({ status: "InvalidStatus" });
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors[0]).toContain('"status" must be one of');
+    });
+
+    it(`DELETE /${API_VERSION}/task/:id should fail with invalid ID`, async () => {
+      const id = "abc";
+
+      const response = await request(app).delete(`/${API_VERSION}/task/${id}`);
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors[0]).toContain('"id" must be a number');
+    });
   });
 });
