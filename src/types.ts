5b8f2a1b6c3daeec7a643a49375007ca5ca82966
aricionur <aricionur@gmail.com>
2025-09-08 16:39:08 +0100

    remove no need interface

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,5 @@
-export interface Task {
-  description?: string;
-  dueDate?: string;
-  status: Status;
-  title: string;
-}
-
 export enum Status {
-  Todo = 'Todo',
-  InProgress = 'In Progress',
-  Done = 'Done',
+  Todo = "Todo",
+  InProgress = "In Progress",
+  Done = "Done",
 }
